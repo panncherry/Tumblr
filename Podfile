8743c608e81c0f35b3da6755f8c06e278563384b
panncherry <maung.eainmatmay@gmail.com>
2018-09-11 00:38:27 -0400

    Solved "No such module AlamofireImage' issue

--- Podfile
+++ Podfile
@@ -4,6 +4,7 @@
 target 'Tumblr' do
   # Comment the next line if you're not using Swift and don't want to use dynamic frameworks
   use_frameworks!
+  pod 'AlamofireImage'
 
   # Pods for Tumblr
 
--- Tumblr/PhotosViewController.swift
+++ Tumblr/PhotosViewController.swift
@@ -12,19 +12,52 @@ import AlamofireImage
 class PhotosViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
     
     var posts: [[String: Any]] = []
+    var refreshControl: UIRefreshControl!
 
     @IBOutlet weak var tableView: UITableView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        let refreshControl = UIRefreshControl()
+        refreshControl.addTarget(self, action: #selector(PhotosViewController.didPullToRefresh(_:)), for: UIControlEvents.valueChanged)
+        tableView.insertSubview(refreshControl, at: 0)
         tableView.delegate = self
         tableView.dataSource = self
-
+        tableView.rowHeight = 275
+        fetchPhotos()
+        }
+    
+    //this method refresh the page and fetch movies
+    @objc func didPullToRefresh(_ refreshControl: UIRefreshControl) {
+        fetchPhotos()
+    }
+    
+    //this method tells how many cells we are going to have
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return posts.count
+    }
+    
+    //this method tells what the cell is going to be
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "PhotoCell", for: indexPath) as! PhotoCell
+        let post = posts[indexPath.row]
+        if let photos = post["photos"] as? [[String: Any]]{
+            let photo = photos[0]
+            let originalSize = photo["original_size"] as! [String: Any]
+            let urlString = originalSize["url"] as! String
+            let url = URL(string: urlString)
+            cell.photoImageView.af_setImage(withURL: url!)
+        }
+        return cell
+    }
+    
+    func fetchPhotos(){
         let url = URL(string: "https://api.tumblr.com/v2/blog/humansofnewyork.tumblr.com/posts/photo?api_key=Q6vHoaVm5L1u2ZAW1fqv3Jw48gFzYVg9P0vH0VHl3GVy6quoGV")!
         
         let request = URLRequest(url: url, cachePolicy: .reloadIgnoringLocalCacheData, timeoutInterval: 10)
         
         let session = URLSession(configuration: .default, delegate: nil, delegateQueue: OperationQueue.main)
+        session.configuration.requestCachePolicy = .reloadIgnoringLocalCacheData
         
         //create a task to get the data
         let task = session.dataTask(with: request){
@@ -38,40 +71,15 @@ class PhotosViewController: UIViewController, UITableViewDataSource, UITableView
                 let responseDictionary = dataDictionary["response"] as! [String: Any]
                 // Store the returned array of dictionaries in our posts property
                 self.posts = responseDictionary["posts"] as! [[String: Any]]
+                self.tableView.reloadData()
+               // self.refreshControl.endRefreshing()
             }
         }
-        task.resume()
+         task.resume()
     }
-    
-    //this method tells how many cells we are going to have
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return posts.count
-    }
-    
-    //this method tells what the cell is going to be
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "PhotoCell", for: indexPath) as! PhotoCell
-        let post = posts[indexPath.row]
-        if let photos = post["photos"] as? [[String: Any]]{
-            //1.
-            let photo = photos[0]
-            // 2.
-            let originalSize = photo["original_size"] as! [String: Any]
-            // 3.
-            let urlString = originalSize["url"] as! String
-            // 4.
-            let url = URL(string: urlString)
-            cell.viewImage.af_setImage(withURL: url!)
-            self.tableView.reloadData()
-        }
-        return cell
-    }
-    
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
 }
