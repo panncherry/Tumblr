76646c2080b265d0b203390b25ca5a80795f0ee7
panncherry <maung.eainmatmay@gmail.com>
2018-09-11 17:02:38 -0400

    infintie scrolling added

--- Tumblr/PhotosViewController.swift
+++ Tumblr/PhotosViewController.swift
@@ -9,10 +9,11 @@
 import UIKit
 import AlamofireImage
 
-class PhotosViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+class PhotosViewController: UIViewController, UITableViewDataSource, UITableViewDelegate, UIScrollViewDelegate {
     
     var posts: [[String: Any]] = []
     var refreshControl: UIRefreshControl!
+    var isMoreDataLoading = false
 
     @IBOutlet weak var tableView: UITableView!
     
@@ -76,6 +77,51 @@ class PhotosViewController: UIViewController, UITableViewDataSource, UITableView
             }
         }
          task.resume()
+    }
+    
+    func loadMoreData() {
+        
+        let url = URL(string: "https://api.tumblr.com/v2/blog/humansofnewyork.tumblr.com/posts/photo?api_key=Q6vHoaVm5L1u2ZAW1fqv3Jw48gFzYVg9P0vH0VHl3GVy6quoGV")!
+        // ... Create the NSURLRequest (myRequest) ...
+        let myRequest = URLRequest(url: url, cachePolicy: .reloadIgnoringLocalCacheData, timeoutInterval: 10)
+        
+        // Configure session so that completion handler is executed on main UI thread
+        let session = URLSession(configuration: URLSessionConfiguration.default,
+                                 delegate:nil,
+                                 delegateQueue:OperationQueue.main
+        )
+        let task : URLSessionDataTask = session.dataTask(with: myRequest, completionHandler: { (data, response, error) in
+            if let error = error {
+                print(error.localizedDescription)
+            } else if let data = data {
+                // Update flag
+                self.isMoreDataLoading = false
+                let dataDictionary = try! JSONSerialization.jsonObject(with: data, options: []) as! [String: Any]
+                print(dataDictionary)
+                // Get the dictionary from the response key
+                let responseDictionary = dataDictionary["response"] as! [String: Any]
+                // Store the returned array of dictionaries in our posts property
+                self.posts = responseDictionary["posts"] as! [[String: Any]]
+                self.tableView.reloadData()
+                //self.refreshControl.endRefreshing()
+            }
+        })
+        task.resume()
+    }
+   
+    func scrollViewDidScroll(_ scrollView: UIScrollView) {
+        if (!isMoreDataLoading) {
+            // Calculate the position of one screen length before the bottom of the results
+            let scrollViewContentHeight = tableView.contentSize.height
+            let scrollOffsetThreshold = scrollViewContentHeight - tableView.bounds.size.height
+            
+            // When the user has scrolled past the threshold, start requesting
+            if(scrollView.contentOffset.y > scrollOffsetThreshold && tableView.isDragging) {
+                isMoreDataLoading = true
+                // ... Code to load more results ...
+                loadMoreData()
+            }
+        }
     }
     
     override func didReceiveMemoryWarning() {
